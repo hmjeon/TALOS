cb8048039da666db43d39f19b9ebf4c2ca98d1f6
Hyungmin Jun <hmjeon@MIT.EDU>
2018-04-30 16:13:39 -0400

    completed

--- src/PERDIX_6P.f90
+++ src/PERDIX_6P.f90
@@ -2,7 +2,7 @@
 ! =============================================================================
 !
 ! PERDIX-6P v1.0
-! Last Updated : 04/10/2018, by Hyungmin Jun (hyungminjun@outlook.com)
+! Last Updated : 04/30/2018, by Hyungmin Jun (hyungminjun@outlook.com)
 !
 ! =============================================================================
 !
